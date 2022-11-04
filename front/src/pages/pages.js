ff9d6bdf03b232df36d512d91876ab6690750f00
tylerse <71523039+tylerse@users.noreply.github.com>
2022-11-04 14:24:07 -0700

    Delete front directory

--- front/src/pages/pages.js
+++
@@ -1,8 +0,0 @@
-import Home from './Home';
-import Customers from './Customers';
-import Houses from './Houses';
-import Employees from './Employees';
-import Costs from './Costs';
-import Investors from './Investors';
-
-export { Home, Customers, Houses, Employees, Costs, Investors };
